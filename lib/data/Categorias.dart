d915776a8202ff15d4d37d21c7584d8314788bfb
carlosdias-ctrl <berseker3120@gmail.com>
2021-08-14 22:54:04 -0300

    projeto estavel

--- lib/data/Categorias.dart
+++ lib/data/Categorias.dart
@@ -4,8 +4,7 @@ const Categorias = {
   '1': const CategoriaDb(
     id: '1',
     name: 'Academias',
-    foto:
-        'https://image.freepik.com/vetores-gratis/conjunto-de-elementos-de-fitness-para-academia_1284-39992.jpg',
+    foto: 'https://exame.com/wp-content/uploads/2019/08/prima-5-3.jpg',
   ),
   '2': const CategoriaDb(
     id: '2',
@@ -17,7 +16,7 @@ const Categorias = {
     id: '3',
     name: 'Farmacias',
     foto:
-        'https://image.freepik.com/vetores-gratis/medicos-e-de-saude-seamless-vetor-cor-textura_1085-294.jpg',
+        'https://thumbs.dreamstime.com/b/s%C3%ADmbolo-da-farm%C3%A1cia-isolado-110821652.jpg',
   ),
   '4': const CategoriaDb(
     id: '4',
@@ -29,12 +28,65 @@ const Categorias = {
     id: '5',
     name: 'Hoteis',
     foto:
-        'https://image.freepik.com/vetores-gratis/hoteis-e-viagens-icons_23-2147498575.jpg',
+        'https://j7e9e8a2.stackpathcdn.com/wp-content/uploads/2015/11/Dicas-de-hot%C3%A9is-em-Amsterd%C3%A3_3.jpg',
   ),
   '6': const CategoriaDb(
     id: '6',
     name: 'Internet',
     foto:
-        'https://image.freepik.com/vetores-gratis/icone-de-tecnologia-de-vetor-de-internet-wi-fi-em-roxo-neon-em-fundo-gradiente_53876-112152.jpg',
+        'https://i2.wp.com/www.telesintese.com.br/wp-content/uploads/2020/05/wifi-joven-internet-acesso-rede-celular.jpg?ssl=1',
+  ),
+  '7': const CategoriaDb(
+    id: '7',
+    name: 'Pizzaria',
+    foto:
+        'https://www.maceioalagoas.com/wp-content/uploads/2018/03/pizza-1-2.jpg',
+  ),
+  '8': const CategoriaDb(
+    id: '8',
+    name: 'Advocacia',
+    foto:
+        'https://jornaldaadvocacia.oabsp.org.br/wp-content/uploads/2021/05/WhatsApp-Image-2021-05-31-at-3.56.53-PM.jpeg',
+  ),
+  '9': const CategoriaDb(
+    id: '9',
+    name: 'Finanças',
+    foto:
+        'https://comoinvestir.thecap.com.br/wp-content/uploads/2021/03/regra-50-30-20-metodo-para-organizar-as-financas-pessoais.jpg',
+  ),
+  '10': const CategoriaDb(
+    id: '10',
+    name: 'Clinicas',
+    foto:
+        'https://telemedicinamorsch.com.br/wp-content/uploads/2017/11/Telemedicina-213017482.jpg',
+  ),
+  '11': const CategoriaDb(
+    id: '11',
+    name: 'AgroNegócio',
+    foto:
+        'https://cdn.brasildefato.com.br/media/5cbc93f0d4cd034f1f5c77f567859f4b.jpg',
+  ),
+  '12': const CategoriaDb(
+    id: '12',
+    name: 'Informática',
+    foto:
+        'https://i1.wp.com/elitecursos.com.br/wp-content/uploads/2015/10/analiseedesenvdesistemas.jpg?fit=540%2C350&ssl=1',
+  ),
+  '13': const CategoriaDb(
+    id: '13',
+    name: 'Supermercado',
+    foto: 'https://www.lages.sc.gov.br/imagens_noticias/1611576652941.jpg',
+  ),
+  '14': const CategoriaDb(
+    id: '14',
+    name: 'Lojas',
+    foto:
+        'https://imagens.ebc.com.br/XMLaHYZ9sPdGOutfdXc3z-93Pds=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/comercio_de_rua_e_saloes_de_beleza_reabrem20200627_0335.jpg?itok=wnC8Zl-L',
+  ),
+  '15': const CategoriaDb(
+    id: '16',
+    name: 'Lanchonete',
+    foto:
+        'https://static.glamurama.uol.com.br/2017/03/tamanho-Lanchonete-da-Cidade-X-S-copy.jpg',
   ),
 };
